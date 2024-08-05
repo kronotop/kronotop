bbc4590cf9199a5d698e85a51ef548a2cd144268
Burak Sezer <burak.sezer.developer@gmail.com>
2024-08-05 10:12:09 +0300

    Add concurrent key appending test to WatchChangesStage
    Introduced a new test method to verify concurrent key appending in the WatchChangesStage. Utilized CountDownLatch, Lock, and ExecutorService to manage concurrent executions and synchronize shared resources. This enhancement ensures the robustness of the replication process under concurrent operations.

--- kronotop/src/test/java/com/kronotop/volume/WatchChangesStageIntegrationTest.java
+++ kronotop/src/test/java/com/kronotop/volume/WatchChangesStageIntegrationTest.java
@@ -24,10 +24,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static org.awaitility.Awaitility.await;
 
@@ -109,15 +117,76 @@ public class WatchChangesStageIntegrationTest extends BaseNetworkedVolumeTest {
             replication.start();
 
             await().atMost(10, TimeUnit.SECONDS).until(() -> replication.getActiveStageRunner() != null);
-
             WatchChangesStageRunner watchChangesStageRunner = (WatchChangesStageRunner) replication.getActiveStageRunner();
-
             await().atMost(10, TimeUnit.SECONDS).until(watchChangesStageRunner::isWatching);
 
             Versionstamp[] versionstampedKeys = appendKeys(10);
             await().atMost(10, TimeUnit.SECONDS).until(() -> checkAppendedEntries(versionstampedKeys, standbyVolume));
         } finally {
             replication.stop();
+        }
+    }
+
+    @Test
+    public void test_watch_changes_stage_concurrently_appending_keys() throws IOException, InterruptedException {
+        Replication replication = newReplication();
+        Volume standbyVolume = standbyVolume();
+        try {
+            replication.start();
+
+            await().atMost(10, TimeUnit.SECONDS).until(() -> replication.getActiveStageRunner() != null);
+            WatchChangesStageRunner watchChangesStageRunner = (WatchChangesStageRunner) replication.getActiveStageRunner();
+            await().atMost(10, TimeUnit.SECONDS).until(watchChangesStageRunner::isWatching);
+
+            CountDownLatch latch = new CountDownLatch(10);
+            List<Versionstamp> versionstampedKeys = new ArrayList<>();
+            Lock lock = new ReentrantLock();
+            try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+                AppendKeysRunnable appendKeysRunnable = new AppendKeysRunnable(versionstampedKeys, lock, latch);
+                for (int i = 0; i < 10; i++) {
+                    executorService.submit(appendKeysRunnable);
+                }
+            }
+            latch.await();
+
+            lock.lock();
+            try {
+                Versionstamp[] result = versionstampedKeys.toArray(Versionstamp[]::new);
+                await().atMost(10, TimeUnit.SECONDS).until(() -> checkAppendedEntries(result, standbyVolume));
+            } finally {
+                lock.unlock();
+            }
+        } finally {
+            replication.stop();
+        }
+    }
+
+    private class AppendKeysRunnable implements Runnable {
+        private final Lock lock;
+        private final List<Versionstamp> versionstampedKeys;
+        private final CountDownLatch latch;
+
+        AppendKeysRunnable(List<Versionstamp> versionstampedKeys, Lock lock, CountDownLatch latch) {
+            this.lock = lock;
+            this.versionstampedKeys = versionstampedKeys;
+            this.latch = latch;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Versionstamp[] result = appendKeys(10);
+                lock.lock();
+                try {
+                    versionstampedKeys.addAll(Arrays.stream(result).toList());
+                } finally {
+                    lock.unlock();
+                }
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            } finally {
+                latch.countDown();
+            }
         }
     }
 }
