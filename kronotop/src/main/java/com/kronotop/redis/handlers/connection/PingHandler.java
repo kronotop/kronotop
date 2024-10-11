0b932c353ba35e13c2c4f6ddfcd00643410a3449
Burak Sezer <burak.sezer.developer@gmail.com>
2024-10-11 19:16:37 +0300

    Move generic handler classes to a new package.
    Renamed handler classes and their tests from 'generic' to 'handlers.generic' to better reflect their roles and improve code organization. This change affects several Java files and their respective package declarations.

---
+++ kronotop/src/main/java/com/kronotop/redis/handlers/connection/PingHandler.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2023-2024 Kronotop
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kronotop.redis.handlers.connection;
+
+import com.kronotop.redis.handlers.connection.protocol.PingMessage;
+import com.kronotop.server.Handler;
+import com.kronotop.server.MessageTypes;
+import com.kronotop.server.Request;
+import com.kronotop.server.Response;
+import com.kronotop.server.annotation.Command;
+import com.kronotop.server.annotation.MaximumParameterCount;
+import com.kronotop.server.resp3.FullBulkStringRedisMessage;
+import io.netty.buffer.ByteBuf;
+
+import java.nio.charset.StandardCharsets;
+
+@Command(PingMessage.COMMAND)
+@MaximumParameterCount(PingMessage.MAXIMUM_PARAMETER_COUNT)
+public class PingHandler implements Handler {
+    final String DEFAULT_PING_RESPONSE = "PONG";
+
+    @Override
+    public void beforeExecute(Request request) {
+        request.attr(MessageTypes.PING).set(new PingMessage(request));
+    }
+
+    @Override
+    public void execute(Request request, Response response) {
+        PingMessage pingMessage = request.attr(MessageTypes.PING).get();
+        if (pingMessage.getMessage() != null) {
+            if (!pingMessage.getMessage().isEmpty() || !pingMessage.getMessage().isBlank()) {
+                ByteBuf buf = response.getChannelContext().alloc().buffer();
+                buf.writeBytes(pingMessage.getMessage().getBytes(StandardCharsets.UTF_8));
+                response.writeFullBulkString(new FullBulkStringRedisMessage(buf));
+                return;
+            }
+        }
+        response.writeSimpleString(DEFAULT_PING_RESPONSE);
+    }
+}
