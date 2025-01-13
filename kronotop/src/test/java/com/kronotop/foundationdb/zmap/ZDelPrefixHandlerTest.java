7298a45acb3ffd8d47886b07a184d8edf00ae62a
Burak Sezer <burak.sezer.developer@gmail.com>
2025-01-13 22:08:08 +0300

    Update licensor name

--- kronotop/src/test/java/com/kronotop/foundationdb/zmap/ZDelPrefixHandlerTest.java
+++ kronotop/src/test/java/com/kronotop/foundationdb/zmap/ZDelPrefixHandlerTest.java
@@ -1,7 +1,7 @@
 package com.kronotop.foundationdb.zmap;
 
 /*
- * Copyright (c) 2023-2025 Kronotop
+ * Copyright (c) 2023-2025 Burak Sezer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
