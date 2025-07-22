c4de7e441d42bf6e381a3cf1a6ce0dd8dc979cda
Burak Sezer <burak.sezer.developer@gmail.com>
2025-07-22 21:06:15 +0300

    Add `IndexDefinition` class and corresponding unit test
    Introduced `IndexDefinition` as a record class with relevant fields and methods to encapsulate index metadata. Added `IndexDefinitionTest` to validate JSON serialization/deserialization and ensure field consistency.

---
+++ kronotop/src/test/java/com/kronotop/bucket/index/IndexDefinitionTest.java
@@ -0,0 +1,32 @@
+// Copyright (C) 2025 Burak Sezer
+// Use of this software is governed by the Business Source License included
+// in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+
+// Change Date: 5 years after release
+
+// On the date above, in accordance with the Business Source License,
+// use of this software will be governed by the open source license specified
+// in the LICENSE.TXT file.
+
+package com.kronotop.bucket.index;
+
+import com.kronotop.internal.JSONUtil;
+import org.bson.BsonType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexDefinitionTest {
+    @Test
+    void shouldEncodeDecode() {
+        IndexDefinition index = new IndexDefinition("index-name", "_id", SortOrder.ASCENDING, BsonType.BINARY);
+        byte[] data = JSONUtil.writeValueAsBytes(index);
+
+        IndexDefinition decoded = JSONUtil.readValue(data, IndexDefinition.class);
+
+        assertEquals(index.name(), decoded.name());
+        assertEquals(index.field(), decoded.field());
+        assertEquals(index.sortOrder(), decoded.sortOrder());
+        assertEquals(index.bsonType(), decoded.bsonType());
+    }
+}
