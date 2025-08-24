358ee13c44ce3c8cd9756504e10fd952aab1e136
Burak Sezer <burak.sezer.developer@gmail.com>
2025-08-24 20:38:16 +0300

    Simplify `PipelineContext` and `IndexScanNode`: remove unused fields, configurations, and methods to streamline pipeline execution logic.

--- kronotop/src/main/java/com/kronotop/bucket/executor/PipelineContext.java
+++ kronotop/src/main/java/com/kronotop/bucket/executor/PipelineContext.java
@@ -16,42 +16,19 @@
 
 package com.kronotop.bucket.executor;
 
-import com.apple.foundationdb.tuple.Versionstamp;
 import com.kronotop.Context;
 import com.kronotop.bucket.BucketMetadata;
-import com.kronotop.bucket.BucketService;
-import com.kronotop.bucket.index.IndexUtil;
 
-import java.nio.ByteBuffer;
-import java.util.LinkedHashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PipelineContext {
     private final Context context;
     private final BucketMetadata metadata;
-    private final PlanExecutorConfig config;
-    private final CursorManager cursorManager;
-    private final FilterEvaluator filterEvaluator;
-    private final DocumentRetriever documentRetriever;
-    private final IndexUtils indexUtils;
-    private final SelectorCalculator selectorCalculator;
-
-    private final ConcurrentHashMap<Integer, LinkedHashMap<Versionstamp, ByteBuffer>> output = new ConcurrentHashMap<>();
-
-    // DAG executor implementation starts here
     private final ConcurrentHashMap<Integer, CursorState> cursor = new ConcurrentHashMap<>();
 
-    public PipelineContext(Context context, BucketMetadata metadata, PlanExecutorConfig config) {
+    public PipelineContext(Context context, BucketMetadata metadata) {
         this.context = context;
         this.metadata = metadata;
-        this.config = config;
-
-        BucketService bucketService = context.getService(BucketService.NAME);
-        this.cursorManager = new CursorManager();
-        this.filterEvaluator = new FilterEvaluator();
-        this.documentRetriever = new DocumentRetriever(bucketService);
-        this.indexUtils = new IndexUtils();
-        this.selectorCalculator = new SelectorCalculator(indexUtils, cursorManager);
     }
 
     public void setCursor(int nodeId, CursorState state) {
@@ -68,34 +45,6 @@ public class PipelineContext {
 
     public BucketMetadata getMetadata() {
         return metadata;
-    }
-
-    public PlanExecutorConfig config() {
-        return config;
-    }
-
-    public SelectorCalculator selectorCalculator() {
-        return selectorCalculator;
-    }
-
-    public CursorManager cursorManager() {
-        return cursorManager;
-    }
-
-    public FilterEvaluator filterEvaluator() {
-        return filterEvaluator;
-    }
-
-    public DocumentRetriever documentRetriever() {
-        return documentRetriever;
-    }
-
-    public IndexUtils indexUtils() {
-        return indexUtils;
-    }
-
-    public LinkedHashMap<Versionstamp, ByteBuffer> getResults(int nodeId) {
-        return output.computeIfAbsent(nodeId, (ignored) -> new LinkedHashMap<>());
     }
 }
 
