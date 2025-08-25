e8f41a1954a30fd92755eeb51fb6041eb11830dc
Burak Sezer <burak.sezer.developer@gmail.com>
2025-08-25 22:23:07 +0300

    Extend `IndexScanPredicate` to support additional BSON types in comparison logic and delegate handling to `FilterEvaluator`.

--- kronotop/src/main/java/com/kronotop/bucket/pipeline/IndexScanPredicate.java
+++ kronotop/src/main/java/com/kronotop/bucket/pipeline/IndexScanPredicate.java
@@ -1,8 +1,6 @@
 package com.kronotop.bucket.pipeline;
 
-import com.kronotop.bucket.bql.ast.BqlValue;
-import com.kronotop.bucket.bql.ast.Int32Val;
-import com.kronotop.bucket.bql.ast.StringVal;
+import com.kronotop.bucket.bql.ast.*;
 import com.kronotop.bucket.planner.Operator;
 
 public record IndexScanPredicate(int id, String selector, Operator op, Object operand) implements Predicate {
@@ -28,6 +26,69 @@ public record IndexScanPredicate(int id, String selector, Operator op, Object op
                     yield false;
                 }
                 yield FilterEvaluator.evaluateComparison(this.op(), actualValue, expectedValue);
+            }
+            case Int64Val(long expectedValue) -> {
+                if (!(bqlValue instanceof Int64Val(long actualValue))) {
+                    yield false;
+                }
+                yield FilterEvaluator.evaluateComparison(this.op(), actualValue, expectedValue);
+            }
+            case DoubleVal(double expectedValue) -> {
+                if (!(bqlValue instanceof DoubleVal(double actualValue))) {
+                    yield false;
+                }
+                yield FilterEvaluator.evaluateComparison(this.op(), actualValue, expectedValue);
+            }
+            case Decimal128Val decimal128Val -> {
+                if (!(bqlValue instanceof Decimal128Val actualDecimal128Val)) {
+                    yield false;
+                }
+                yield FilterEvaluator.evaluateComparison(this.op(), actualDecimal128Val.value(), decimal128Val.value());
+            }
+            case BooleanVal(boolean expectedValue) -> {
+                if (!(bqlValue instanceof BooleanVal(boolean actualValue))) {
+                    yield false;
+                }
+                yield FilterEvaluator.evaluateComparison(this.op(), actualValue, expectedValue);
+            }
+            case NullVal ignored -> {
+                yield !(bqlValue instanceof NullVal);
+            }
+            case BinaryVal(byte[] expectedValue) -> {
+                if (!(bqlValue instanceof BinaryVal(byte[] actualValue))) {
+                    yield false;
+                }
+                yield FilterEvaluator.evaluateComparison(this.op(), actualValue, expectedValue);
+            }
+            case TimestampVal(long expectedValue) -> {
+                if (!(bqlValue instanceof TimestampVal(long actualValue))) {
+                    yield false;
+                }
+                yield FilterEvaluator.evaluateComparison(this.op(), actualValue, expectedValue);
+            }
+            case DateTimeVal(long expectedValue) -> {
+                if (!(bqlValue instanceof DateTimeVal(long actualValue))) {
+                    yield false;
+                }
+                yield FilterEvaluator.evaluateComparison(this.op(), actualValue, expectedValue);
+            }
+            case VersionstampVal versionstampVal -> {
+                if (!(bqlValue instanceof VersionstampVal actualVersionstampVal)) {
+                    yield false;
+                }
+                yield FilterEvaluator.evaluateComparison(this.op(), actualVersionstampVal.value().getBytes(), versionstampVal.value().getBytes());
+            }
+            case ArrayVal arrayVal -> {
+                if (!(bqlValue instanceof ArrayVal actualArrayVal)) {
+                    yield false;
+                }
+                yield !arrayVal.values().equals(actualArrayVal.values());
+            }
+            case DocumentVal documentVal -> {
+                if (!(bqlValue instanceof DocumentVal actualDocumentVal)) {
+                    yield false;
+                }
+                yield !documentVal.fields().equals(actualDocumentVal.fields());
             }
             default -> throw new IllegalStateException("Unexpected value: " + this.operand());
         };
