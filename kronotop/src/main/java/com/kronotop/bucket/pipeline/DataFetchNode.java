573447f3bcc24f6122d18ce54edf274d6874b743
Burak Sezer <burak.sezer.developer@gmail.com>
2025-08-26 21:42:07 +0300

    Remove `DataFetchNode` and `DataFetchNodeImpl` as they are no longer in use.

--- kronotop/src/main/java/com/kronotop/bucket/pipeline/DataFetchNode.java
+++
@@ -1,8 +0,0 @@
-package com.kronotop.bucket.pipeline;
-
-import java.util.List;
-
-public interface DataFetchNode extends PipelineNode {
-    List<PipelineNode> children();
-    void execute(PipelineContext ctx);
-}
