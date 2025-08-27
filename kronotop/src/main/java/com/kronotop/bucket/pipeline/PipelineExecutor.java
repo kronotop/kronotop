8902278f198487c463ef86baa935daba43e331c3
Burak Sezer <burak.sezer.developer@gmail.com>
2025-08-27 16:39:39 +0300

    Refactor `PipelineExecutor` to support distinct handling of `FullScanNode` with a dedicated `executeFullScanNode` method, update execution logic to switch based on node types.

--- kronotop/src/main/java/com/kronotop/bucket/pipeline/PipelineExecutor.java
+++ kronotop/src/main/java/com/kronotop/bucket/pipeline/PipelineExecutor.java
@@ -4,6 +4,7 @@ import com.apple.foundationdb.Transaction;
 import com.apple.foundationdb.tuple.Versionstamp;
 
 import java.nio.ByteBuffer;
+import java.nio.channels.Pipe;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,13 +16,13 @@ public class PipelineExecutor {
         this.root = root;
     }
 
-    public Map<Versionstamp, ByteBuffer> execute(Transaction tr, PipelineContext ctx) {
-        Map<Integer, DocumentLocation> locations = ctx.output().getLocations(root.id());
+    private Map<Versionstamp, ByteBuffer> executeAndFetchDocuments(Transaction tr, PipelineContext ctx, PipelineNode node) {
+        Map<Integer, DocumentLocation> locations = ctx.output().getLocations(node.id());
         if (locations == null || locations.size() < ctx.limit()) {
-            executeNode(tr, ctx, root);
+            executeNode(tr, ctx, node);
         }
         if (locations == null) {
-            locations = ctx.output().getLocations(root.id());
+            locations = ctx.output().getLocations(node.id());
         }
 
         Map<Versionstamp, ByteBuffer> results = new LinkedHashMap<>();
@@ -42,9 +43,41 @@ public class PipelineExecutor {
             iterator.remove();
         }
         if (results.isEmpty()) {
-            ctx.output().clearLocations(root.id());
+            ctx.output().clearLocations(node.id());
         }
         return results;
+    }
+
+    private Map<Versionstamp, ByteBuffer> executeFullScanNode(Transaction tr, PipelineContext ctx, PipelineNode node) {
+        executeNode(tr, ctx, node);
+
+        Map<Versionstamp, ByteBuffer> documents = ctx.output().getDocuments(node.id());
+        if (documents.isEmpty()) {
+            return null;
+        }
+
+        Map<Versionstamp, ByteBuffer> results = new LinkedHashMap<>();
+        int counter = 0;
+        for (Iterator<Map.Entry<Versionstamp, ByteBuffer>> iterator = documents.entrySet().iterator(); iterator.hasNext(); ) {
+            if (counter >= ctx.limit()) {
+                break;
+            }
+            Map.Entry<Versionstamp, ByteBuffer> entry = iterator.next();
+            results.put(entry.getKey(), entry.getValue());
+            counter++;
+            iterator.remove();
+        }
+        if (results.isEmpty()) {
+            ctx.output().removeDocuments(node.id());
+        }
+        return results;
+    }
+
+    public Map<Versionstamp, ByteBuffer> execute(Transaction tr, PipelineContext ctx) {
+        return switch (root) {
+            case FullScanNode node -> executeFullScanNode(tr, ctx, node);
+            default -> executeAndFetchDocuments(tr, ctx, root);
+        };
     }
 
     private void executeNode(Transaction tr, PipelineContext ctx, PipelineNode node) {
