7356f10cffbaf2dd341efe66c1931762b4c15f26
Burak Sezer <burak.sezer.developer@gmail.com>
2025-08-29 18:29:46 +0300

    Add `testUnionWithTwoFieldReverse` to verify OR query execution with reverse ordering in `UnionNodeWithFullScanStrategyTest`.

--- kronotop/src/test/java/com/kronotop/bucket/pipeline/UnionNodeWithFullScanStrategyTest.java
+++ kronotop/src/test/java/com/kronotop/bucket/pipeline/UnionNodeWithFullScanStrategyTest.java
@@ -6,6 +6,7 @@ import com.kronotop.bucket.BucketMetadata;
 import org.junit.jupiter.api.Test;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -70,6 +71,47 @@ public class UnionNodeWithFullScanStrategyTest extends BasePipelineTest {
             assertEquals(4, results.size());
             assertEquals(Set.of("Claire", "John", "Alison"), extractNamesFromResults(results));
             assertEquals(Set.of(35, 40, 47), extractIntegerFieldFromResults(results, "age"));
+        }
+    }
+
+    @Test
+    void testUnionWithTwoFieldReverse() {
+        final String TEST_BUCKET_NAME = "test-intersection-full-scan-strategy";
+
+        BucketMetadata metadata = createIndexesAndLoadBucketMetadata(TEST_BUCKET_NAME);
+
+        // Insert multiple documents with different field types and values
+        List<byte[]> documents = List.of(
+                BSONUtil.jsonToDocumentThenBytes("{'age': 20, 'name': 'John'}"), // match
+                BSONUtil.jsonToDocumentThenBytes("{'age': 23, 'name': 'Alice'}"),
+                BSONUtil.jsonToDocumentThenBytes("{'age': 25, 'name': 'George'}"),
+                BSONUtil.jsonToDocumentThenBytes("{'age': 35, 'name': 'Claire'}"), // match
+                BSONUtil.jsonToDocumentThenBytes("{'age': 35, 'name': 'John'}"), // match
+                BSONUtil.jsonToDocumentThenBytes("{'age': 40, 'name': 'Alison'}"), // match
+                BSONUtil.jsonToDocumentThenBytes("{'age': 47, 'name': 'John'}") // match
+        );
+
+        insertDocumentsAndGetVersionstamps(TEST_BUCKET_NAME, documents);
+
+        PipelineExecutor executor = createPipelineExecutorForQuery(metadata, "{ $or: [ { 'age': { '$gt': 25 } }, { 'name': { '$eq': 'John' } } ] }");
+        PipelineContext ctx = createPipelineContext(metadata);
+        ctx.setReverse(true);
+
+        List<String> expectedResult = List.of(
+                "{\"age\": 47, \"name\": \"John\"}",
+                "{\"age\": 40, \"name\": \"Alison\"}",
+                "{\"age\": 35, \"name\": \"John\"}",
+                "{\"age\": 35, \"name\": \"Claire\"}",
+                "{\"age\": 20, \"name\": \"John\"}"
+        );
+
+        try (Transaction tr = context.getFoundationDB().createTransaction()) {
+            Map<?, ByteBuffer> results = executor.execute(tr, ctx);
+            List<String> actualResult = new ArrayList<>();
+            for (ByteBuffer buffer : results.values()) {
+                actualResult.add(BSONUtil.fromBson(buffer.array()).toJson());
+            }
+            assertEquals(expectedResult, actualResult);
         }
     }
 }
