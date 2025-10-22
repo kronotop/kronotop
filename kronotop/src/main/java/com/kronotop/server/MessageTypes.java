46a6aa8d7085a1a45c0edab7a1d662bc1109431d
Burak Sezer <burak.sezer.developer@gmail.com>
2025-10-22 19:31:50 +0300

    Add support for `BUCKET.INDEX` command with a new handler and subcommand parsing
    - Introduced `BucketIndexHandler` to handle the new `BUCKET.INDEX` command.
    - Added `BucketIndexMessage` and `BucketIndexSubcommand` classes for command parsing and subcommand logic.
    - Updated `BucketService`

--- kronotop/src/main/java/com/kronotop/server/MessageTypes.java
+++ kronotop/src/main/java/com/kronotop/server/MessageTypes.java
@@ -143,6 +143,7 @@ public class MessageTypes {
     public static final AttributeKey<BucketDeleteMessage> BUCKETDELETE = AttributeKey.valueOf(BucketDeleteMessage.COMMAND);
     public static final AttributeKey<BucketUpdateMessage> BUCKETUPDATE = AttributeKey.valueOf(BucketUpdateMessage.COMMAND);
     public static final AttributeKey<BucketCloseMessage> BUCKETCLOSE = AttributeKey.valueOf(BucketCloseMessage.COMMAND);
+    public static final AttributeKey<BucketIndexMessage> BUCKETINDEX = AttributeKey.valueOf(BucketIndexMessage.COMMAND);
 
     // Session management
     public static final AttributeKey<SessionAttributeMessage> SESSIONATTRIBUTE = AttributeKey.valueOf(SessionAttributeMessage.COMMAND);
